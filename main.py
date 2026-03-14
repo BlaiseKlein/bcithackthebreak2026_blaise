e4a6defd318b77b7b707c105d54c7f630f32818e
Joe Lin <jlin249@my.bcit.ca>
2026-03-14 15:28:55 -0700

    fixed gitignore

---
+++ main.py
@@ -0,0 +1,9 @@
+from src.openai_service import get_ai_bot_response
+
+def main():
+    prompt = input("Ask about a command: ")
+    response = get_ai_bot_response(prompt)
+    print(response)
+
+if __name__ == "__main__":
+    main()
