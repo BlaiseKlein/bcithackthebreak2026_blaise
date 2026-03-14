b39f49c81fc3241303abe38ab9bf6743320fc9b3
Kohei Dunnet <kdunnet1@my.bcit.ca>
2026-03-14 13:04:12 -0700

    Ping and SCP values passed back through dictionary

--- GUI/screens/scp.py
+++ GUI/screens/scp.py
@@ -1,6 +1,6 @@
 from textual.app import ComposeResult
 from textual.containers import Horizontal, VerticalScroll
-from textual.widgets import Button, Header, Footer, Input, SelectionList
+from textual.widgets import Button, Header, Footer, Input, SelectionList,TextArea
 from textual.screen import Screen
 from textual import on
 
@@ -11,19 +11,19 @@ class SPCScreen(Screen):
         yield Horizontal(
             VerticalScroll(
                 SelectionList[int](
-                        ("Port", 0),
-                        ("Limit Bandwidth", 1),
-                        ("Compress Data", 2),
-                        ("Copy Subdirectories", 3),
-                        ("Verbose", 4),
-                        ("Private Key", 5),
+                        ("Port (-P)", 0),
+                        ("Limit Bandwidth (-L)", 1),
+                        ("ompression (-C)", 2),
+                        ("Copy Subierctories Recursive (-r)", 3),
+                        ("Verbose(-v)", 4),
+                        ("Private Key(-i)", 5),
                         id="optionId"
                 ),
-                Input(placeholder = "Filename",),
-                Input(placeholder = "Username",),
-                Input(placeholder = "IP",),
-                Input(placeholder = "Target Directory",),
-                Input(placeholder = "Target Machine Password",),
+                Input(placeholder = "Filename",id="file"),
+                Input(placeholder = "Username",id="user"),
+                Input(placeholder = "IP",id="ip"),
+                Input(placeholder = "Target Directory",id="directory"),
+                Input(placeholder = "Target Machine Password",id="passwpr"),
                 Input(placeholder = "Port", disabled=True, id="portId"),
                 Input(placeholder = "Bandwidth Limit", disabled=True, id="bandwidthId"),
                 Input(placeholder = "Private Key", disabled=True, id="privateKeyId")
@@ -31,13 +31,16 @@ class SPCScreen(Screen):
             ),
             VerticalScroll(
                 Button("Go to main Menu", id="menuBtn"),
-                Button("Start", id="startBtn"),
-            )
+                Button("Submit", id="submitBtn"),
+            ),
+
+            TextArea("",id ="textArea")
+            
         )
         yield Footer()
 
     @on(Button.Pressed,"#menuBtn")
-    def on_button_pressed(self, event: Button.Pressed) -> None:
+    def onButtonPressed(self, event: Button.Pressed) -> None:
         self.app.pop_screen()
 
     @on(SelectionList.SelectedChanged, "#optionId")
@@ -47,6 +50,38 @@ class SPCScreen(Screen):
         self.query_one("#portId").disabled = 0 not in selectedIndices
         self.query_one("#bandwidthId").disabled = 1 not in selectedIndices
         self.query_one("#privateKeyId").disabled = 5 not in selectedIndices
+    
+    @on(Button.Pressed, "#submitBtn")
+    def onSubmit(self, event: Button.Pressed)-> None:
+        allOptions = [
+        {"index": 0, "label": "Port"},
+        {"index": 1, "label": "Bandwidth"},
+        {"index": 2, "label": "Compression"},
+        {"index": 3, "label": "Recursive"},
+        {"index": 4, "label": "Verbose"},
+        {"index": 5, "label": "Private Key"},
+        ]
+
+        inputValues = {
+            input.id: input.value 
+            for input in self.query(Input)
+            if input.value
+        }
+
+        selectedOptions = self.query_one("#optionId", SelectionList).selected 
+
+        options = []   
+        for option in allOptions:
+            if option["index"] in selectedOptions:
+                options.append(option[""])
+            
+        inputValues["options"] = options
+        
+        inputStr = str(inputValues)
+
+        textArea = self.query_one("#textArea", TextArea)
+        textArea.text = inputStr
+        
             
 
 
